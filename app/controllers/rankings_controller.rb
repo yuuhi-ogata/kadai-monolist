2b2470965e5110ea2c34426b8f0df0cfe50d000b
EC2 Default User <ec2-user@ip-172-31-15-138.us-east-2.compute.internal>
2018-04-25 11:57:36 +0000

    ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,5 @@
 class RankingsController < ApplicationController
-  class RankingsController < ApplicationController
+  
   def want
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
@@ -8,6 +8,5 @@ class RankingsController < ApplicationController
   def have
     @ranking_counts = Have.ranking
     @items = Item.find(@ranking_counts.keys)
-  end
   end
 end
